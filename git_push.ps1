eb9d942744a8cd938ff0c98626bfd445ab476fb0
Heitor Boccato <heitor.boccato@amorsaude.local>
2025-08-01 15:30:07 -0300

    AtualizaÃ§Ã£o automÃ¡tica

---
+++ git_push.ps1
@@ -0,0 +1,11 @@
+# Adiciona todos os arquivos
+git add .
+
+# Mostra o status
+git status
+
+# Faz commit com mensagem
+git commit -m "Atualização automática"
+
+# Faz push para o repositório remoto
+git push -u origin main
